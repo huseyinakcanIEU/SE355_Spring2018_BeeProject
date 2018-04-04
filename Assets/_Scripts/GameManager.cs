b1440268bf92e17505d054caf1a504ad7e8275a6
Arda Zeytin <ardazeytin@outlook.com>
2018-04-05 01:58:53 +0300

    GameManager Singleton implemented

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    #region Singleton GameManager
+
+    private static GameManager _instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                GameObject gm = new GameObject("GameManager");
+                gm.AddComponent<GameManager>();
+            }
+
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    #endregion
+}
