05e7196088827aff6bbbbbd87d0c7787fe50486d
Simge Özcan <simgeozcan95@gmail.com>
2018-04-04 17:08:36 +0300

    Singleton GUI implemented

---
+++ Bee Game GUI/Assets/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour 
+{
+
+	public GameObject optpanel;
+
+	private static GameManager _instance;
+
+	public static GameManager Instance 
+	{
+
+		get 
+		{
+
+			if (_instance == null) 
+			{
+				GameObject go = new GameObject ("Game Manager");
+				go.AddComponent<GameManager> ();
+			}
+
+			return _instance;
+
+		}
+
+	}
+
+	public void HostButton()
+	{
+		Debug.Log ("Host Button called");
+	}
+
+	public void JoinButton()
+	{
+		Debug.Log ("Join Button called");
+	}
+
+	public void OptionButton()
+	{
+		Debug.Log ("Option Button called");
+		optpanel.gameObject.SetActive (true);
+
+	}
+
+
+	void Awake()
+	{
+		_instance = this;
+	}
+
+}
---
+++ Bee Game GUI/Assets/Scripts/TestObjectScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestObjectScript : MonoBehaviour 
+{
+	void Start()
+	{
+		
+	}
+
+	public void HostButtonCaller()
+	{
+		GameManager.Instance.HostButton ();
+	}
+
+	public void JoinButtonCaller()
+	{
+		GameManager.Instance.JoinButton ();
+	}
+
+	public void OptionButtonCaller()
+	{
+		GameManager.Instance.OptionButton ();
+	}
+
+}
