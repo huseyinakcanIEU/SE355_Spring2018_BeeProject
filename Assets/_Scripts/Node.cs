e03cf3eb1eaecf2c61a3a27f3b8c649f0ab62690
Arda Zeytin <ardazeytin@outlook.com>
2018-04-05 03:23:36 +0300

    new tags for nodes, enum implement

---
+++ Assets/_Scripts/Node.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public int concurentBee = 0;
+    public string nodeType = null;
+
+
+
+    enum NodeType
+    {
+        Resource,
+        Control,
+    }
+
+    private void Awake()
+    {
+        if (gameObject.CompareTag("ResourceNode"))
+        {
+            nodeType = NodeType.Resource.ToString();
+        }
+        else if (gameObject.CompareTag("ControlNode"))
+        {
+            nodeType = NodeType.Control.ToString();
+        }
+    }
+}
