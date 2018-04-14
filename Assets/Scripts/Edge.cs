1bd772e1d6333f288907b4713b386be72704578a
ErkinKurt <kurt.erkin@gmail.com>
2018-04-14 19:16:05 +0300

    Graph initialization.
    Vertex, Edge and GraphManager scripts has been added.

---
+++ Assets/Scripts/Edge.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Edge : ScriptableObject {
+
+	
+		public int weight = 0;
+		public Vertex vertex;
+	
+		public Edge(int weight,Vertex vertex)
+		{
+			this.weight = weight;
+			this.vertex = vertex;
+		}
+	
+}
