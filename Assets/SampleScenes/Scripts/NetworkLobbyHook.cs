35b0fbf0132138c7074d033d59363cf2f42c4398
Arda Zeytin <ardazeytin@outlook.com>
2018-04-25 16:25:20 +0300

    Revert "Working multiplayer without client to server sync"
    This reverts commit 748bc562bacb15931990b48a5a3ca36f7c68e9e5.

--- Assets/SampleScenes/Scripts/NetworkLobbyHook.cs
+++ Assets/SampleScenes/Scripts/NetworkLobbyHook.cs
@@ -8,10 +8,7 @@ public class NetworkLobbyHook : LobbyHook
     //lobideki oyuncu icin oyun sahnesi yuklenirken yapilacaklar
     public override void OnLobbyServerSceneLoadedForPlayer(NetworkManager manager, GameObject lobbyPlayer, GameObject gamePlayer)
     {
-        //GameManager.Instance.playerList.Add(gamePlayer); //lobideki oyuncuyu gameManager'deki oyuncularin listesine ekle
-
-        //Oyuncunun sectigi rengi al
-        //oyuncunun koydugu ismi al
+        GameManager.Instance.playerList.Add(gamePlayer); //lobideki oyuncuyu gameManager'deki oyuncularin listesine ekle
 
     }
 }
