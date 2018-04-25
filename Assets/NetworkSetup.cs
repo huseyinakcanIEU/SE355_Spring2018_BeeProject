7b1fa58cbaa57c7df73bd46500cc923341de2511
Arda Zeytin <ardazeytin@outlook.com>
2018-04-25 16:25:44 +0300

    Revert "Network players"
    This reverts commit 7232850f002298e6852255b49699da66ca8de847.

---
+++ Assets/NetworkSetup.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkSetup : NetworkBehaviour {
+
+    public GameObject[] createBeeUI;
+
+    public override void OnStartClient()
+    {
+        //GameObject createBeeUI = Instantiate()
+    }
+}
