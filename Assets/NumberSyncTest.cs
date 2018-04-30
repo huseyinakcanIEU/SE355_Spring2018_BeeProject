5e16c06cb7945b3bc5f595c56ad2989cb6257e5b
Arda Zeytin <ardazeytin@outlook.com>
2018-04-30 14:15:26 +0300

    SyncTest

---
+++ Assets/NumberSyncTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NumberSyncTest : NetworkBehaviour
+{
+    [SyncVar(hook = "IncrementSyncCallBack")]
+    public int testNumber = 0;
+
+
+    public void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            IncrementSyncCallBack(1);
+        }
+
+        Debug.Log("TestNumber " + testNumber);
+    }
+
+    //[Command]
+    public void IncrementSyncCallBack(int newNumber)
+    {
+        if (isServer)
+        {
+            return;
+        }
+        testNumber = testNumber + newNumber;
+    }
+}
